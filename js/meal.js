c95ed3c424c4e5e123076a1681e2bf045eb30f3d
devruhul <ruhulamin262001@gmail.comm>
2021-09-01 01:38:39 +0600

    image, css and js file updated nad final commit

--- js/meal.js
+++ js/meal.js
@@ -1,3 +1,5 @@
+
+// get search button
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -8,7 +10,7 @@ const searchFood = () => {
         .then(res => res.json())
         .then(data => displayResult(data.meals));
 };
-
+// display result
 const displayResult = meals => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
@@ -25,4 +27,29 @@ const displayResult = meals => {
         `;
         searchResult.appendChild(div);
     });
+};
+// load each meal detail
+const loadMealDetail = mealId => {
+    const url = `
+        https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}
+    `;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]));
+    // display meal details
+    const displayMealDetails = meal => {
+        const mealDetails = document.getElementById('meal-details');
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+            <img  src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+                <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+            </div>
+        `;
+        mealDetails.appendChild(div);
+    };
+
 };
